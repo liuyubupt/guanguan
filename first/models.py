a98499cd460beff5e7033f76968f6e1cdb57aa09
LxxxJ <630285616@qq.com>
2020-05-28 22:55:25 +0800

    Update

--- first/models.py
+++ first/models.py
@@ -1,3 +1,99 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    nid = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=32,unique=True,null=False)
+    password = models.CharField(max_length=32,null=False)
+    active_day = models.IntegerField(default=0)
+    last_act = models.CharField(max_length=1000,default="1970-01-01")
+    created_time = models.DateTimeField(auto_now = True)
+    updated_time = models.DateTimeField(auto_now = True)
+    class Meta:
+        db_table = "guan_UserInfo"
+
+class ActivityType(models.Model):
+    nid = models.AutoField(primary_key=True)
+    act_type = models.CharField(max_length=32,unique=True,null=False)
+    created_time = models.DateTimeField(auto_now = True)
+    updated_time = models.DateTimeField(auto_now = True)
+    class Meta:
+        db_table = "guan_ActivityType"
+
+class Activity(models.Model):
+    nid = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
+    nfc = models.CharField(max_length=1000,null=False)
+    type_id = models.IntegerField()
+    act_name = models.CharField(max_length=1000,null=False)
+    created_time = models.DateTimeField(auto_now=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = "guan_Activity"
+
+class ActSta(models.Model):
+    nid = models.AutoField(primary_key=True)
+    act_id = models.IntegerField()
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
+    moment_text = models.CharField(max_length=1000,default="")
+    is_shared = models.IntegerField(default=0)
+    shared_time = models.DateTimeField(auto_now=True)
+    created_time = models.DateTimeField(auto_now = True)
+    updated_time = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = "guan_ActSta"
+
+class Box(models.Model):
+    nid = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
+    nfc = models.CharField(max_length=1000,null=False)
+    box_name = models.CharField(max_length=1000,null=False)
+    box_pos = models.CharField(max_length=1000, null=False)
+    created_time = models.DateTimeField(auto_now=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = "guan_Box"
+
+class BoxContent(models.Model):
+    nid = models.AutoField(primary_key=True)
+    box_id = models.IntegerField()
+    thing_name = models.CharField(max_length=1000,null=False)
+    thing_num = models.IntegerField()
+    created_time = models.DateTimeField(auto_now=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = "guan_BoxContent"
+
+class Friend(models.Model):
+    nid = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
+    friend_id = models.IntegerField()
+    created_time = models.DateTimeField(auto_now=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = "guan_Friend"
+
+class Application(models.Model):
+    nid = models.AutoField(primary_key=True)
+    from_id = models.IntegerField()
+    to_id = models.IntegerField()
+    content = models.CharField(max_length=1000,null=False,blank=False)
+    is_processed = models.IntegerField(default=0)
+    created_time = models.DateTimeField(auto_now=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = "guan_Application"
+
+
+class PushNote(models.Model):
+    nid = models.AutoField(primary_key=True)
+    author_id = models.IntegerField()
+    title = models.CharField(max_length=1000,null=False,blank=False)
+    summary = models.CharField(max_length=1000,null=False,blank=False)
+    contents = models.CharField(max_length=10000,null=False,blank=False)
+    created_time = models.DateTimeField(auto_now=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = "guan_PushNote"
